ed5422e3ffc871ecd94ac810f1df3cb511539148
johwen <johwenchen@163.com>
2017-04-08 21:56:23 +0800

    add test case

--- src/test/scala/example/HelloSpec.scala
+++ src/test/scala/example/HelloSpec.scala
@@ -1,9 +1,20 @@
 package example
 
 import org.scalatest._
+import collection.mutable.Stack
+
 
 class HelloSpec extends FlatSpec with Matchers {
   "The Hello object" should "say hello" in {
     Hello.greeting shouldEqual "hello"
   }
+  
+  "A Stack" should "pop values in last-in-first-out order" in {
+    val stack = new Stack[Int]
+    stack.push(1)
+    stack.push(2)
+    stack.pop() should be (2)
+    stack.pop() should be (2)
+  }
+
 }
